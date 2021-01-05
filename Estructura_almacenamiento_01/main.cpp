a9f944c998adfdb10c8f9d48bb23bad5fc728cfc
Krikorotojava <73000191+Krikorotojava@users.noreply.github.com>
2021-01-05 12:07:57 -0600

    Add files via upload

--- Estructura_almacenamiento_01/main.cpp
+++ Estructura_almacenamiento_01/main.cpp
@@ -1,22 +1,18 @@
 #include<stdio.h>
-struct perro{
-    char nombre[30];
-    int edadMeses;
-    float peso;
-}perros[3];
-int main(){
-    for (int i=0; i < 3; ++i){
-     printf("%iIngresa el nombre del perro\n",i+1);
-     scanf("s",&perros[i].nombre);
-     printf("%iIngresa la edad del perro:\n",i+1);
-     scanf("%i",&perros[i].edadMeses);
-     printf("%iIngrese el peso del perro\n",i+1);
-     scanf("%f",&perros[i].peso);
-    }
-    for (int i = 0; i < 3; ++i){
-        printf("%i El nombre del perro es %s\n",i+1,perros[i].nombre);
-        printf("%i La edad del perro es: %s\n",i+1,perros[i].edadMeses);
-        printf("%i El peso del perro es: %.2f\n",i+1,perros[i].peso);
-    }
-    return 0;
+main(){
+    char a[100];
+    int b;
+    float c;
+    //almacenamiento de datos
+    printf("Hola, introduzca lo que se te pide:\n");
+    printf("Nombre: \n");
+    scanf("%s",&a);
+    printf("\n un numero entero: ");
+    scanf("%i",&b);
+    printf("Un numero con decimal: \n");
+    scanf("%f",&c);
+    //Impresion de datos
+    printf("hola %s:3",a);
+    printf("Su numero entero es: %i\n",b);
+    printf("tu numero decimal es : %f",c);
 }
